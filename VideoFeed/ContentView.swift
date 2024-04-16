f082109ede5b1a2d290b51b7c981b89fd01e554c
Alessandra F. Lacerda <flacerdaale@gmail.com>
2024-04-15 21:55:34 -0300

    Initial Commit

---
+++ VideoFeed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoFeed
+//
+//  Created by Alessandra Fernandes Lacerda on 15/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoFeed/VideoFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFeedApp.swift
+//  VideoFeed
+//
+//  Created by Alessandra Fernandes Lacerda on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
